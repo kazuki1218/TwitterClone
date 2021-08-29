7b436cc77ff79773e1f6ce21b2582c9e18e407e7
kazuki1218 <v.shiotakazuki.1218@gmail.com>
2021-08-29 15:04:08 +0900

    会員登録機能

--- config.php
+++ config.php
@@ -6,4 +6,8 @@ date_default_timezone_set('Asia/TOkyo');
 //URL/ディレクトリ設定
 define('HOME_URL', 'http://localhost/TwitterClone/');
 // ('/TwitterClone/')だけでもよい
-
+// データベースの接続情報
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'twitter_clone');
