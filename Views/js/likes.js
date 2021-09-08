8f7e6988c8231c3bc7a70b75c18a0bffe22e603a
kazuki1218 <v.shiotakazuki.1218@gmail.com>
2021-09-08 15:42:51 +0900

    いいね！機能を非同期で実装＆検索機能

--- Views/js/likes.js
+++ Views/js/likes.js
@@ -1,32 +1,66 @@
 ///////////////////////////////////////
 // いいね！用のJavaScript
 ///////////////////////////////////////
-$(function(){
-    // いいね！がクリックされた時
-    $('.js-like').click(function(){
-        const this_obj = $(this); //クリックされた時
-        const like_id = $(this).data('like-id'); //クリックされた時('like-id')の数が入ります。
-        const like_conut_obj = $(this).parent().find('.js-like-count');
-        let like_count = Number(like_conut_obj.html());
 
-        if(like_id){
-            //いいねの取り消し
-            //いいね！いいね！カウントを減らす
-            like_count--;
-            like_conut_obj.html(like_count);
-            this_obj.data('like-id', null);
+$(function () {
+    // いいね！がクリックされたとき
+    $('.js-like').click(function () {
+        const this_obj = $(this);
+        const tweet_id = $(this).data('tweet-id');
+        const like_id = $(this).data('like-id');
+        const like_count_obj = $(this).parent().find('.js-like-count');
+        let like_count = Number(like_count_obj.html());
 
-            //いいね！いいね！ボタンの色をグレーに変更
-            $(this).find('img').attr('src','../Views/img/icon-heart.svg');
-        } else{
-            //いいねを付与
-            //いいね！いいね！カウントを増やす
-            like_count++;
-            like_conut_obj.html(like_count);
-            this_obj.data('like-id', true);
+        if (like_id) {
+            // いいね！取り消し
+            // 非同期通信
+            $.ajax({
+                url: 'like.php',
+                type: 'POST',
+                data: {
+                    'like_id': like_id
+                },
+                timeout: 10000
+            })
+                // 取り消しが成功
+                .done(() => {
+                    // いいね！カウントを減らす
+                    like_count--;
+                    like_count_obj.html(like_count);
+                    this_obj.data('like-id', null);
 
-            //いいね！いいね！ボタンの色を青に変更
-            $(this).find('img').attr('src','../Views/img/icon-heart-twitterblue.svg');
+                    // いいね！ボタンの色をグレーに変更
+                    $(this).find('img').attr('src', '../Views/img/icon-heart.svg');
+                })
+                .fail((data) => {
+                    alert('処理中にエラーが発生しました。');
+                    console.log(data);
+                });
+        } else {
+            // いいね！付与
+            // 非同期通信
+            $.ajax({
+                url: 'like.php',
+                type: 'POST',
+                data: {
+                    'tweet_id': tweet_id
+                },
+                timeout: 10000
+            })
+                // いいね！が成功
+                .done((data) => {
+                    // いいね！カウントを増やす
+                    like_count++;
+                    like_count_obj.html(like_count);
+                    this_obj.data('like-id', data['like_id']);
+
+                    // いいね！ボタンの色を青に変更
+                    $(this).find('img').attr('src', '../Views/img/icon-heart-twitterblue.svg');
+                })
+                .fail((data) => {
+                    alert('処理中にエラーが発生しました。');
+                    console.log(data);
+                });
         }
     });
 })
