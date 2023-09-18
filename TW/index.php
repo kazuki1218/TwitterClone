e6a1c9dc944dfe334bae6660727b5b599591464f
kazuki1218 <85749291+kazuki1218@users.noreply.github.com>
2023-09-18 18:09:38 +0900

    TWディレクトリの追加（特に何もなし）

---
+++ TW/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="../Views/img/logo-twitterblue.svg">
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <link href="style.css" rel="stylesheet">
+
+        <title>ホーム画面 / Twitterクローン</title>
+        <meta name="description" content="ホーム画面です">
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="side">
+                <div class="side-inner">
+                    <ul class="nav flex-column">
+                        <li class="nav-item"><a href="home.php" class="nav-link"><img
+                                    src="../Views/img/logo-twitterblue.svg" alt=""></a></li>
+                        <li class="nav-item"><a href="home.php" class="nav-link"><img src="../Views/img/icon-home.svg"
+                                    alt=""></a></li>
+                        <li class="nav-item"><a href="search.php" class="nav-link"><img
+                                    src="../Views/img/icon-search.svg" alt=""></a></li>
+                        <li class="nav-item"><a href="notification.php" class="nav-link"><img
+                                    src="../Views/img/icon-notification.svg" alt=""></a></li>
+                        <li class="nav-item"><a href="profile.php" class="nav-link"><img
+                                    src="../Views/img/icon-profile.svg" alt=""></a></li>
+                        <li class="nav-item"><a href="post.php" class="nav-link"><img
+                                    src="../Views/img/icon-post-tweet-twitterblue.svg" alt=""></a></li>
+                        <li class="nav-item"><img src="../Views/img_uploaded/user/sample-person.jpg" alt=""></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="main"></div>
+        </div>
+    </body>
+
+</html>
